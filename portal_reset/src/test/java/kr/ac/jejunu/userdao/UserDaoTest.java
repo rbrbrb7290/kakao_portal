410e51970b33efea45df3050496cb59ad7fd35f9
rbrbrb7290 <ohg429@gmail.com>
2019-04-18 20:11:55 +0900

    #tag_5 Spring Core만들기

--- portal_reset/src/test/java/kr/ac/jejunu/userdao/UserDaoTest.java
+++ portal_reset/src/test/java/kr/ac/jejunu/userdao/UserDaoTest.java
@@ -1,6 +1,9 @@
 package kr.ac.jejunu.userdao;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.*;
 
@@ -9,11 +12,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserDaoTest {
-
-    public void setup() {
-
+    private UserDao userDao;
+    @Before
+    public void setup(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao" , UserDao.class);
     }
-
     //입력한 id에 속한 name ,password Data가 일치하는지 확인하는 test
     @Test
     public void get() throws SQLException, ClassNotFoundException {
@@ -21,11 +25,6 @@ public class UserDaoTest {
         Long id = 1l;
         String name = "오현규";
         String password = "1234";
-
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
-        //UserDao userDao = new UserDao(new JejuConnectionMaker());
-
 
         User user = userDao.get(id);
         //db를 연결해서 저장한 데이터가 일치하는지 확인 (assertEquals(id user.getId())도 가능)
@@ -40,7 +39,6 @@ public class UserDaoTest {
         String name = "kakao!";
         String password = "1234";
 
-        UserDao userDao = new UserDao(new JejuConnectionMaker());
         User user = new User();
 
         user.setName(name);
@@ -55,19 +53,19 @@ public class UserDaoTest {
 
     }
 
-    @Test
-    public void getHalla() throws SQLException, ClassNotFoundException {
-        Long id = 1l;
-        String name = "허윤호";
-        String password = "1234";
-        UserDao userDao = new UserDao(new HallaConnectionMaker());
-
-
-        User user = userDao.get(id);
-        //db를 연결해서 저장한 데이터가 일치하는지 확인 (assertEquals(id user.getId())도 가능)
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
-    }
+//    @Test
+//    public void getHalla() throws SQLException, ClassNotFoundException {
+//        Long id = 1l;
+//        String name = "허윤호";
+//        String password = "1234";
+//        UserDao userDao = new UserDao(new HallaConnectionMaker());
+//
+//
+//        User user = userDao.get(id);
+//        //db를 연결해서 저장한 데이터가 일치하는지 확인 (assertEquals(id user.getId())도 가능)
+//        assertThat(user.getId(), is(id));
+//        assertThat(user.getName(), is(name));
+//        assertThat(user.getPassword(), is(password));
+//    }
 
 }
